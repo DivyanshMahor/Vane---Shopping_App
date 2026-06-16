8c94ede302abb4cb46351cc43a8b0f0c8f80778e
Divyansh <divyanshmahor3@gmail.com>
2026-06-16 23:56:15 +0530

    Add search filter feature

--- lib/features/products/screen/product_list_screen.dart
+++ lib/features/products/screen/product_list_screen.dart
@@ -4,21 +4,29 @@ import 'package:flutter_assignment/features/products/provider/product_provider.d
 import 'package:flutter_assignment/features/products/screen/product_details_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProductListScreen extends ConsumerWidget {
+class ProductListScreen extends ConsumerStatefulWidget {
   const ProductListScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final productState = ref.watch(productProvider);  // iske ander loading, error and DATA bhi aa rah h
+  ConsumerState<ProductListScreen> createState() => _ProductListScreenState();
+}
+
+class _ProductListScreenState extends ConsumerState<ProductListScreen> {
+  String input = "";  //isme search value ayegi
+  @override
+  Widget build(BuildContext context) {
+    final productState = ref.watch(
+      productProvider,
+    ); // iske ander loading, error and DATA bhi aa rah h
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
       home: Scaffold(
         backgroundColor: AppColors.roseWood,
         appBar: AppBar(
-
-          backgroundColor: AppColors.roseWood , //text ke background ka color
-          leading: Padding(padding:const EdgeInsetsGeometry.all(8),
+          backgroundColor: AppColors.roseWood, //text ke background ka color
+          leading: Padding(
+            padding: const EdgeInsetsGeometry.all(8),
             child: CircleAvatar(
               backgroundImage: AssetImage("assets/image/img1.jpg"),
             ),
@@ -26,47 +34,97 @@ class ProductListScreen extends ConsumerWidget {
           centerTitle: true,
           title: Text(
             "Vané",
-            style: TextStyle(fontSize: 32,fontWeight: FontWeight.w500, fontStyle:
-            FontStyle.italic,
-                color: AppColors.vanilla),),), // text ka color
-    body: productState.when(
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w500,
+              fontStyle: FontStyle.italic,
+              color: AppColors.vanilla,
+            ),
+          ),
+        ), // text ka color
+        body:
 
-        data: (productsList){
-      return ListView.builder(
-          itemCount: productsList.length,
-          itemBuilder: (context,index){
-            final product = productsList[index];
-            return 
-              Card(
-                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                elevation: 6,
-              shadowColor: AppColors.butterCupYellow,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),),
-                color: AppColors.vanilla,
+        productState.when(
+          data: (productsList) {
+            final filteredList = productsList.where((product){
+              return product.title.toLowerCase().contains(input.toLowerCase());
+            }).toList();
 
-              child: ListTile(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> ProductDetailsScreen( product:product  ) ));
-              },
-                leading: Image.network(product.thumb,
-                width: 60,
-                  height: 60,
-                  fit: BoxFit.cover,
-                ),
-                title: Text(product.title),
-                subtitle: Text("\$ ${product.price}"),
-              ),
-            );
 
-          } );
-    },
-        error: (err,stack)=> Center(child: Text("Error: $err")) ,
+            return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child:
 
-        loading: ()=> const Center(
-          child: CircularProgressIndicator(),
-        ) ),
-    ),
+                    TextField(
+                      onChanged: (value){
+                        setState(() {
+                          input = value;
+                        });
+                      }
+
+                      ,decoration: InputDecoration(
+                      labelText: "Search",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40),
+                      )
+                    ),),
+                  ),
+
+                  Expanded(
+                    child: ListView.builder(
+                    itemCount:
+
+                    filteredList.length,
+
+                    itemBuilder: (context, index) {
+                      final product = productsList[index];
+
+                      return Card(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 10,
+                        ),
+                        elevation: 6,
+                        shadowColor: AppColors.butterCupYellow,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        color: AppColors.vanilla,
+
+                        child: ListTile(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    ProductDetailsScreen(product: product),
+                              ),
+                            );
+                          },
+                          leading: Image.network(
+                            product.thumb,
+                            width: 60,
+                            height: 60,
+                            fit: BoxFit.cover,
+                          ),
+                          title: Text(product.title),
+                          subtitle: Text("\$ ${product.price}"),
+                        ),
+                      );
+                    },
+                                ),
+                  ),
+                ],
+              );
+          },
+          error: (err, stack) => Center(child: Text("Error: $err")),
+          loading: () => const Center(child: CircularProgressIndicator()),
+        ),
+
+
+      ),
     );
   }
 }
